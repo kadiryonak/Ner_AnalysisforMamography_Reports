4203f49be39567d5b28130a73fe6a7cf7351e2b2
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2024-09-27 13:02:26 +0300

    Update dataset.py

--- Ner_AnalysisforMamography_Reports/dataset.py
+++ Ner_AnalysisforMamography_Reports/dataset.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
-# Custom Dataset sınıfı
+# Custom Dataset class
 class NERDataset(Dataset):
     def __init__(self, bio_data, word_to_ix, tag_to_ix):
         self.data = bio_data
@@ -18,7 +18,7 @@ class NERDataset(Dataset):
         tag_idxs = [self.tag_to_ix[tag] for tag in tags]
         return torch.tensor(word_idxs, dtype=torch.long), torch.tensor(tag_idxs, dtype=torch.long)
 
-# DataLoader için collate function
+# DataLoader for collate function
 def collate_fn(batch):
     sentences, tags = zip(*batch)
     sentences_padded = pad_sequence(sentences, batch_first=True, padding_value=word_to_ix["<PAD>"])
